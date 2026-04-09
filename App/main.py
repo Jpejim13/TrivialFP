22d6484b8126f2e4c1cbaa2b55de95e18fa8a6d0
Jorge Peña <jpenajim@gmail.com>
2026-04-09 19:40:47 +0200

    Arreglo de la ventana y creacion del metodo para navegar

---
+++ App/main.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+
+class MainWindow(QtCore.QObject):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        current_dir = os.path.dirname(__file__)
+        ui_path = os.path.join(current_dir, "juego.ui")
+
+        ui_file = QFile(ui_path)
+        if not ui_file.open(QFile.ReadOnly):
+            print(f"No se pudo abrir el archivo en: {ui_path}")
+            return
+
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        # IMPORTANTE: En tu UI se llama "Mazo"
+        self.pantallas = self.window.pantallas
+        self.pantallas.setCurrentIndex(0)
+
+        # Conexiones de botones
+        self.window.BotonJugar.clicked.connect(lambda: self.on_clicked_cambio_pantalla("elegirModo"))
+        #se lo pasamos como la lambda pq por defecto la funcion on clicked pasa true o false entonces no podriamos meterle el argumento "elegir modo", la funcion
+        #lambda nos permite pasarle el string 
+        
+        self.window.show()
+        
+    def on_clicked_cambio_pantalla(self,pantalla):
+        minuscula = pantalla.lower()
+        match minuscula: # Sin paréntesis aquí
+            case "elegirmodo": # Importante: comparamos contra minúsculas
+                self.pantallas.setCurrentIndex(1)
+            case "ayuda":
+                print("Mostrando guía de usuario.")
+            case "salir":
+                print("Cerrando aplicación.")
+            case _:
+                print("Comando no reconocido.")
+        
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+sys.exit(app.exec())
